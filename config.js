7e03307a6456e97e1fcfb1fc53373c75385d65bf
Md. Yamin Hossain <needyamin@gmail.com>
2021-10-08 10:45:30 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
           //FireBase Database Config	
 		  var config = {
-			  apiKey: " AAAAESDdNRg:APA91bFoA13GgEqSapjoBaz7NlSyMhiPk-5BJOILY2PZBPQHc2569rCVyLNzhs1gQHTfJmMElHIzSl7kXR3SFpyshYYVylOAkOEiNzQic0hNR7fLBW6InxrR-3Go2T8BQhrlo0vXbLJG ",
-			  authDomain: "ansnew007.firebaseapp.com",
-			  databaseURL: "https://ansnew007-default-rtdb.firebaseio.com",
-			  storageBucket: "ansnew007.appspot.com"
+			  apiKey: "Please Paste your key Here",
+			  authDomain: "******.firebaseapp.com",
+			  databaseURL: "https://******-default-rtdb.firebaseio.com",
+			  storageBucket: "******.appspot.com"
 			  };
 			  firebase.initializeApp(config);
 			  
